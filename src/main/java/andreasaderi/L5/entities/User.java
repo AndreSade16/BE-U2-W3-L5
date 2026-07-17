f869d15372687b5cfb4dfaa08512fa7b6c6b7347
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-17 10:21:10 +0200

    entities created

---
+++ src/main/java/andreasaderi/L5/entities/User.java
@@ -0,0 +1,62 @@
+package andreasaderi.L5.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+@JsonIgnoreProperties({"password"})
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    @Column(name = "user_id")
+    private UUID userId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(nullable = false, name = "profile_pic_url")
+    private String profilePicUrl;
+
+    public User(String name, String surname, String email, String password, Role role) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.profilePicUrl = "https://ui-avatars.com/api/?name=" + name + "+" + surname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
